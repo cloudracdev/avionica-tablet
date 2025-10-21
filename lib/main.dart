30d783f78bffc650bf07ed8130272b0fd86a113b
Bruno Raniere <rm555765@fiap.com.br>
2025-10-21 17:16:55 -0300

    codigos separados importando elemento vermelho

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/sixpack_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,14 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'Hello, World 👋',
-            style: TextStyle(fontSize: 28),
-          ),
-        ),
-      ),
+      home: SixPackScreen(),
     );
   }
 }
---
+++ lib/screens/sixpack_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SixPackScreen extends StatelessWidget {
+  const SixPackScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 150,
+          height: 150,
+          color: Colors.redAccent,
+        ),
+      ),
+    );
+  }
+}
